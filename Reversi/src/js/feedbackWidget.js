15005abfce2668d4d21b75646dd85e28106391c7
PW\pw <patrickwobben@live.nl>
2022-09-21 20:07:49 +0200

    Updated Feedback Widget

--- Reversi/src/js/feedbackWidget.js
+++ Reversi/src/js/feedbackWidget.js
@@ -4,6 +4,12 @@ class FeedbackWidget{
      this._element = document.getElementById(this._elementId);
 
      $(this._element).hide()
+     $(this._element).find('.feedbackClose button')
+      .on('click', () => {
+        $(this._element).removeClass('show');
+
+        setTimeout(() => $(this._element).hide(), 1500);
+      })
    }
    
   get elementId() { //getter, set keyword voor setter methode
@@ -11,8 +17,9 @@ class FeedbackWidget{
   }
     
   show(message, type){
-    $(this._element).text(message);
+    $(this._element).find('.feedbackMessage p').text(message);
     $(this._element).addClass(type);
+    $(this._element).addClass('show');
     $(this._element).show();
 
     this.log({ message, type })
@@ -48,17 +55,17 @@ class FeedbackWidget{
 
 
  $(() => {
-    const error = new FeedbackWidget('error');
+//     const error = new FeedbackWidget('error');
     const challenge = new FeedbackWidget('challenge');
-    const koffie = new FeedbackWidget('koffie');
+//     const koffie = new FeedbackWidget('koffie');
 
-    error.show("Je gegevens konden niet worden opgehaald. Zo jammer. Probeer het later nog eens.", "error");
+//     error.show("Je gegevens konden niet worden opgehaald. Zo jammer. Probeer het later nog eens.", "error");
     challenge.show("Mike wil deelnemen aan jouw spel. Geef akkoord.", "success");
 
-    koffie.show(
-      'Bijna klaar, tijd voor koffie',
-      'success'
-    );
+//     koffie.show(
+//       'Bijna klaar, tijd voor koffie',
+//       'success'
+//     );
 
-    console.log(koffie.history());
+//     console.log(koffie.history());
   });
--- Reversi/src/js/index.js
+++ Reversi/src/js/index.js
@@ -15,15 +15,26 @@ const apiUrl = 'url/super/duper/game';
 const Game = ((apiUrl) => {
   
   const configMap = {
-    apiUrl
+    apiUrl: '',
+    token: ''
   };
+
+  const stateMap = {
+    gameState: 0
+  }
 
   // Private function list
   const init = (afterInit) => {
     console.log(configMap.apiUrl);
 
+    getCurrentGameState();
     afterInit();
   }
+
+  const getCurrentGameState = () =>
+    setInterval(() => {
+      stateMap.gameState = Game.Model.getGameState(configMap.token);
+    }, 2000)
 
   // Waarde/object geretourneerd aan de outer scope
   return {
@@ -47,16 +58,54 @@ Game.Reversi = (() => {
 })();
 
 Game.Data = (() => {
-  const configMap = {
+  let configMap = {
+    mock: [
+      {
+          url: 'api/Spel/Beurt',
+          data: 0
+      }
+    ],
+    apiKey: ''
+  };
 
+  let stateMap = {
+    environment: 'development'
   }
 
-  const init = () => {
+  const init = (environment) => {
     console.log('private init Data');
+
+    if (environment) {
+      if (environment == 'development' || environment == 'production') {
+        stateMap.environment = environment;
+      } else {
+        throw new Error("Environment must be 'development' or 'production'")
+      }
+    }
   }
+
+  const getData = (url) =>  
+    $.get(url)
+        .then(r => {
+            return r
+            })
+        .catch(e => {
+            console.log(e.message);
+        });  
+
+  const getMockData = (url) =>
+    new Promise((resolve, reject) => {
+        resolve(configMap.mock);
+    });
+
+  const get = (url) => 
+    stateMap.environment == 'production' 
+      ? getData(url) 
+      : getMockData(url);
 
   return {
-    init
+    init,
+    get
   }
 })();
 
@@ -69,8 +118,19 @@ Game.Model = (() => {
     console.log('private init Model');
   }
 
+
+  const getGameState = (token) =>
+    Game.Data.get(`.api/Spel/Beart/${token}`)
+      .then(gameState => {
+        if (gameState < 0 || gameState > 2) {
+          throw new Error('GameState is invalide')
+        }
+        return gameState;
+      });
+
   return {
-    init
+    init,
+    getGameState
   }
 })();
 
